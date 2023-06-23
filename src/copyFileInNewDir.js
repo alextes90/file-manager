067acc49e42f7ed4cc05f7be89c33cf53ddf9fc0
Aleksandr <a.teslin90@gmail.com>
2023-06-23 16:24:39 +0300

    feat: add copyFile logic

---
+++ src/copyFileInNewDir.js
@@ -0,0 +1,41 @@
+import { createReadStream, createWriteStream } from "fs";
+import fsPromise from "fs/promises";
+import { pipeline } from "stream/promises";
+import path from "path";
+import { pathToSource } from "./utils/index.js";
+
+export const copyFileToNewDir = async (
+  pathToCurrentDir,
+  pathToFile,
+  newFileDir
+) => {
+  try {
+    const pathToFileToCopy = pathToSource(pathToCurrentDir, pathToFile);
+    const pathToNewDir = pathToSource(pathToCurrentDir, newFileDir);
+    const fileName = path.basename(pathToFileToCopy);
+    const pathToNewFile = path.join(pathToNewDir, fileName);
+
+    const isOldPathDir = (await fsPromise.stat(pathToFileToCopy)).isDirectory();
+    let isNewFileAlreadyExist = false;
+
+    try {
+      await fs.access(pathToNewFile);
+      isNewFileAlreadyExist = true;
+    } catch {}
+
+    if (isOldPathDir || isNewFileAlreadyExist) {
+      throw new Error("");
+    }
+
+    const readStream = createReadStream(pathToFileToCopy);
+    const writeStream = createWriteStream(pathToNewFile);
+
+    await pipeline(readStream, writeStream);
+
+    readStream.on("error", () => {
+      throw new Error();
+    });
+  } catch {
+    console.error("FS operation failed");
+  }
+};
