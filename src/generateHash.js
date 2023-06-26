b56b595173a459a45fb588adb367cb45e00e75b6
Aleksandr <a.teslin90@gmail.com>
2023-06-26 19:01:12 +0300

    fix: fix hash counting logic

--- src/generateHash.js
+++ src/generateHash.js
@@ -3,18 +3,24 @@ import { pathToSource } from "./utils/index.js";
 
 const { createHash } = await import("crypto");
 
-const hash = createHash("sha256");
-
 export const generateHash = async (pathToCurrentDir, pathToFile) => {
   return new Promise((resolve) => {
     const purePath = pathToSource(pathToCurrentDir, pathToFile);
     const readableStream = createReadStream(purePath);
 
+    const helperArr = [];
+
     readableStream
+      .on("data", (data) => {
+        helperArr.push(data.toString());
+      })
       .on("error", () => console.log("Operation failed"))
-      .on("close", resolve)
-      .pipe(hash)
-      .setEncoding("hex")
-      .on("data", (data) => console.log(data.toString()));
+      .on("close", () => {
+        if (helperArr.length !== 0) {
+          const hash = createHash("sha256");
+          console.log(hash.update(helperArr.join()).digest("hex"));
+        }
+        resolve();
+      });
   });
 };
